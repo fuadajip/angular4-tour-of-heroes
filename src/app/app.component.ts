6c5ad8d4e23d4903e69afaa5e963efa1d690218c
fuadajip <fuadajipratomo@gmail.com>
2017-03-28 00:13:18 +0700

    refractor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+export class Hero {
+  id : number;
+  name : String;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template : '<h1>{{title}}</h1> and the hero is <h2> {{hero.name}} </h2>'
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'We are awesome heroes!';
+  hero : Hero = {
+    id : 1,
+    name :'Simba'
+  };
 }
+
+
+
