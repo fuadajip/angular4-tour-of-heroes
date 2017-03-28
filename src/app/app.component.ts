f74f0c6fceab626fbac8929657614ae70193fb60
fuadajip <fuadajipratomo@gmail.com>
2017-03-28 09:23:35 +0700

    Heroes detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,27 @@ export class Hero {
   id : number;
   name : String;
 }
-
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template : '<h1>{{title}}</h1> and the hero is <h2> {{hero.name}} </h2>'
+
 })
 export class AppComponent {
   title = 'We are awesome heroes!';
-  hero : Hero = {
-    id : 1,
-    name :'Simba'
-  };
+  heroes = HEROES;
 }
 
 
